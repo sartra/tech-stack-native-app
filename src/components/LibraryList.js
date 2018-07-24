c106fb824d17831c524de53c2de8dc42159fb7c6
Sarah Renshaw <srenshaw@nycl137457m.us.dci.discovery.com>
2018-07-24 17:21:47 -0400

    error messages

---
+++ src/components/LibraryList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'; 
+import { connect } from 'react-native';
+import { FlatList } from 'react-native';   // renders only components that are currently visible in the list 
+import ListItem from './ListItem'; 
+
+class LibraryList extends Component {
+
+  renderItem(library) {
+    return <ListItem library={library} />;
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+      <FlatList 
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={(library) => library.id}
+      /> 
+    )
+  }
+}
+
+// map some elements of global state to props to component LibraryList - what is returned from this func is sent to Lib List as props
+const mapStateToProps = state => {
+  console.log('state:', state)
+  return { libraries: state.libraries}
+}
+
+export default connect(mapStateToProps)(LibraryList);
